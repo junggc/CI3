aaa9d1523795ec07153fa098b294d02c9750f796
kacelab <jung@kacelab.com>
2022-03-18 17:54:17 +0900

    Initial commit

---
+++ application/controllers/Tutorial.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tutorial extends CI_Controller {
+
+    public function index()
+    {
+        echo "Hello Index";
+    }
+
+    public function second()
+    {
+        echo "Hello Second";
+    }
+}
