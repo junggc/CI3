c20565a2ba582c7d75be88f1e93f8a6205f8ef3f
kacelab <jung@kacelab.com>
2022-03-21 18:41:22 +0900

    뷰 관련

--- application/controllers/Tutorial.php
+++ application/controllers/Tutorial.php
@@ -6,11 +6,25 @@ class Tutorial extends CI_Controller
 
     public function index()
     {
-        $this->load->view('index');
+        $data = array(
+            'title' => 'Tutorial::index',
+            'content' => 'Hello Index'
+        );
+        $this->load->view('Tutorial/header', $data);
+        $this->load->view('Tutorial/content', $data);
+        $this->load->view('Tutorial/footer', $data);
     }
 
     public function second()
     {
-        $this->load->view('second');
+        $data = array(
+            'title' => 'Tutorial::second',
+            'content' => 'Hello Second'
+        );
+
+        $this->load->view('Tutorial/header', $data); // view/Tutorial 에 header.php
+        $this->load->view('Tutorial/content', $data); // view/Tutorial 에 content.php
+        $this->load->view('Tutorial/footer', $data);  // view/Tutorial 에 footer.php
     }
 }
+
